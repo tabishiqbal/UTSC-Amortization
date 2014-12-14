456944ceb99d7abdc716427e8b1891ddb79f6c82
Lazigerbill <chaukbill@gmail.com>
2014-12-14 15:46:37 -0500

    Highcharts added

--- app/assets/javascripts/interface.js
+++ app/assets/javascripts/interface.js
@@ -44,6 +44,32 @@ angular.module('amortization', [])
 				principal: $scope.totalPrincipal,
 				balance: balance
 			});
+
+			$(function () { 
+			    $('#container').highcharts({
+			        chart: {
+			            type: 'bar'
+			        },
+			        title: {
+			            text: 'Fruit Consumption'
+			        },
+			        xAxis: {
+			            categories: ['Apples', 'Bananas', 'Oranges']
+			        },
+			        yAxis: {
+			            title: {
+			                text: 'Fruit eaten'
+			            }
+			        },
+			        series: [{
+			            name: 'Jane',
+			            data: [1, 0, 4]
+			        }, {
+			            name: 'John',
+			            data: [5, 7, 3]
+			        }]
+			    });
+			});
 		};
 
 		$scope.calInterestPortion = function(balance) {
